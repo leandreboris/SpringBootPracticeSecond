031a3c944e3feb039a4c740c7636411783b38b11
Léandre <leandre.wangrawa@berexia.com>
2022-03-27 17:38:36 +0000

    Initial commit

---
+++ src/main/java/com/optimagrowth/license/SpringPracticeSecondApplication.java
@@ -0,0 +1,33 @@
+package com.optimagrowth.license;
+
+import java.util.Locale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@SpringBootApplication
+public class SpringPracticeSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeSecondApplication.class, args);
+	}
+
+	@Bean
+	public SessionLocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setUseCodeAsDefaultMessage(true);
+		messageSource.setBasenames("messages");
+		return messageSource;
+	}
+
+}
